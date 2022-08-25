609900131fa6fe2154e9b0b1629da7ebef09a6be
behnamesmi <behnam.esmi.dev@gmail.com>
2022-08-26 00:56:38 +0430

    Add_loginForm

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
+  // userName = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+  };
+  handleSubmit = (e) => {
+    const account = this.state.account;
+    console.log(account);
+    e.preventDefault();
+  };
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
-          </div>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+            type="password"
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
